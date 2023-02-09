dcc13d2d049437ad66b146fb13cdb07242ec1a88
Papierkorb <62933268+PapierKorb76@users.noreply.github.com>
2023-02-09 22:10:00 +0100

    Implementing Card Shuffle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import "./Card.scss";
+import "./card.scss";
 import Card from "./Card";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import cardArray from "./cardArray";
 
 export default function App() {
+  let [shuffle, setShuffle] = useState(false);
+
+  if (shuffle === true) {
+    setShuffle(false);
+  }
+
   useEffect(() => {
-    console.log("ciao");
-  }, []);
+    console.log(cardArray);
+  }, [shuffle]);
+
+  function shuffleCards(e) {
+    var i = 0,
+      j = 0,
+      temp = null;
+    for (i = cardArray.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = cardArray[i];
+      cardArray[i] = cardArray[j];
+      cardArray[j] = temp;
+    }
+    setShuffle(true);
+  }
 
   const style = document.body.style;
   useEffect(() => {
     style.margin = "0px";
+    style.padding = "0px";
     style.width = "100%";
     style.height = "100vh";
   });
@@ -19,7 +39,9 @@ export default function App() {
     <div className="App">
       <div className="card-holder">
         {cardArray.map((card) => (
-          <Card sourceImg={card.sourceImg} text={card.text} key={card.id} />
+          <div key={card.id} onClick={shuffleCards}>
+            <Card sourceImg={card.sourceImg} text={card.text} key={card.id} />
+          </div>
         ))}
       </div>
     </div>
--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Card.scss";
+import "./card.scss";
 
 export default function Card({ sourceImg, text }) {
   return (
